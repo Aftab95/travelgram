d7b164412a692e868d6b991ef2e3b0c40d241360
Mohammad aftab <aftabmohammad008@gmail.com>
2021-02-21 14:55:43 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDEtDRdJW6yAN9mOGZGYSZH8dXQJPnj77c",
+    authDomain: "react-31-eb200.firebaseapp.com",
+    projectId: "react-31-eb200",
+    storageBucket: "react-31-eb200.appspot.com",
+    messagingSenderId: "467296842871",
+    appId: "1:467296842871:web:7b6c47129f7539878fc272"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,30 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase: {
+//   apiKey: "AIzaSyDEtDRdJW6yAN9mOGZGYSZH8dXQJPnj77c",
+//   authDomain: "react-31-eb200.firebaseapp.com",
+//   projectId: "react-31-eb200",
+//   storageBucket: "react-31-eb200.appspot.com",
+//   messagingSenderId: "467296842871",
+//   appId: "1:467296842871:web:7b6c47129f7539878fc272"
+// },
+
+
+
+firebase: {
+  apiKey: "AIzaSyAE9FrieraK3Zk3gBR44BR3I-0dKFEJsB0",
+  authDomain: "angular-3-8ec14.firebaseapp.com",
+  projectId: "angular-3-8ec14",
+  storageBucket: "angular-3-8ec14.appspot.com",
+  messagingSenderId: "792822623747",
+  appId: "1:792822623747:web:f463d62c903270e7d32836"
+},
 };
 
 /*
